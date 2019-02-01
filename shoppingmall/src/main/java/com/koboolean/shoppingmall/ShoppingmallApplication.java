b440dca3d6602a3b64be93c85418093a8443b667
USER <USER@DESKTOP-359E9BG>
2019-02-01 23:44:18 +0900

    - 기본화면 셋팅

--- shoppingmall/src/main/java/com/koboolean/shoppingmall/ShoppingmallApplication.java
+++ shoppingmall/src/main/java/com/koboolean/shoppingmall/ShoppingmallApplication.java
@@ -1,7 +1,10 @@
-package com.example.demo;
+package com.koboolean.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ShoppingmallApplication {
